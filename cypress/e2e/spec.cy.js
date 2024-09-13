48e892561763348ba5c096683c5a4058edd4b83a
Nasveta Camdzic <nasveta.c@gmail.com>
2024-09-13 09:22:12 +0200

    Added changes into the structure

--- cypress/e2e/spec.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Visit Staging BalkanTransfer', () => {
-  it('should load the BalkanTransfer staging homepage', () => {
-    cy.visit('/');  // Using relative path because baseUrl is already set
-    cy.get('h1').should('be.visible');  // Example: Check if the header is visible
-  });
-});
