08f40c518c75b9970bab33205d517a8280a2e6ff
Nasveta Camdzic <nasveta.c@gmail.com>
2024-10-10 19:10:39 +0200

    added results and jUnit config

--- cypress.config.js
+++ cypress.config.js
@@ -4,12 +4,7 @@ module.exports = defineConfig({
   e2e: {
     chromeWebSecurity: false,
     testIsolation: false,
-    baseUrl: 'https://staging.balkantransfer.com/',
-    reporter: "cypress-junit-reporter",
-    reporterOptions: {
-      "mochaFile": "results/junit-[hash].xml",
-      "toConsole": true
-    }
-  }
+    baseUrl: 'https://staging.balkantransfer.com/'
+  },
 
 });
