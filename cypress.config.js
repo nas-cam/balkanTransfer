dab052e382316c00602ff0c48b93efb109ad92d5
Nasveta Camdzic <nasveta.c@gmail.com>
2024-10-10 19:32:55 +0200

    add new file

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,20 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      // Register the JUnit reporter
+      on('after:run', (results) => {
+        return cypressJunit(results);
+      });
+    },
     chromeWebSecurity: false,
     testIsolation: false,
     baseUrl: 'https://staging.balkantransfer.com/'
+  },
+  reporter: 'cypress-junit-reporter',
+  reporterOptions: {
+    mochaFile: 'results/junit-[hash].xml',
+    toConsole: true,
   },
 
 });
--- cypress/plugins/index.js
+++
@@ -1,7 +0,0 @@
-const cypressJunit = require('cypress-junit-reporter');
-
-module.exports = (on, config) => {
-    on('after:run', (results) => {
-        return cypressJunit(results);
-    });
-};
