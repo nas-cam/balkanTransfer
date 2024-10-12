766e42e24a7e086c85418e383b93c126fd96520c
Mersed <mersed.camdzic@gmail.com>
2024-10-12 20:47:58 +0200

    Regression tests refactored

--- cypress/e2e/regression/headerLinksVerification.cy.js
+++ cypress/e2e/regression/headerLinksVerification.cy.js
@@ -5,11 +5,9 @@ const data = require('../../data/balkanTransferData.json');
 
 describe('Regression User Side Test Suite', () => {
     const homePage = new HomePage();
-    const searchResultsPage = new SearchResultsPage();
 
     beforeEach(() => {
         homePage.visitHomePage();
-        verifyDiscoveredLinkAndPageHeader(homePage, homePage.clickOnLanguageDropdown, homePage.clickOnEnglishLanguage, homePage.getLanguageDropdown,'ENG');
         homePage.getWelcomeHeader().should('contain.text', 'Welcome');
     });
 
@@ -40,25 +38,5 @@ describe('Regression User Side Test Suite', () => {
         verifyDiscoveredLinkAndPageHeader(homePage, homePage.clickOnLanguageDropdown, homePage.clickOnEnglishLanguage, homePage.getLanguageDropdown,'ENG');
         verifyDiscoveredLinkAndPageHeader(homePage, homePage.clickOnLanguageDropdown, homePage.clickOnBosnianLanguage, homePage.getLanguageDropdown,'BOS');
         verifyDiscoveredLinkAndPageHeader(homePage, homePage.clickOnLanguageDropdown, homePage.clickOnEnglishLanguage, homePage.getLanguageDropdown,'ENG');
-    });
-
-    it('should populate from and to fields add pick-up address, should verify search results and query data', () => {
-        homePage.enterFromLocationIntoSearchForm(data.searchFormData.from);
-        homePage.selectDropDownFrom();
-        homePage.enterToLocationIntoSearchForm(data.searchFormData.to);
-        homePage.selectDropdownTo();
-        homePage.clickOnDate();
-        homePage.selectDay();
-        homePage.clickOnSearchButton();
-        searchResultsPage.visitSearchResultPage();
-        searchResultsPage.getSearchResultsHeader()
-            .should('be.visible')
-            .should('contain', 'Select your pick-up address');
-        searchResultsPage.insertPickupAddress(data.pickupAddress.pickupAddress);
-        searchResultsPage.getPickupDropdown();
-        searchResultsPage.clickOnConfirmButton();
-
-        searchResultsPage.getSearchQueryHeader().contains(data.searchFormData.from);
-        searchResultsPage.getSearchQueryHeader().contains(data.searchFormData.to);
     });
 });
